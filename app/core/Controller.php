066a1695a33acc81962e4b997d9d528cb145fe11
Ovdiychuk Oleksandr <agraceful1616@gmail.com>
2018-05-29 19:51:04 +0300

    0.1.0

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -11,11 +11,15 @@ abstract class Controller
     public $route; // массив из названия контроллера и єкшна для доступа внутри любого из контроллеров
     public $view;
     public $model = [];
+    public $acl;
 
     public function __construct($route)
     {
 
         $this->route = $route; // присвоение значения роута, переданного при візове контроллера в маршрутизаторе
+        if (!$this->checkAcl()) {
+            View::errorCode(403);
+        }
         $this->view = new View($route);
 
     }
@@ -29,6 +33,31 @@ abstract class Controller
             return new $path();
 
         }
+
+    }
+
+    public function checkAcl() {
+
+        $this->acl = require 'app/acl/'.$this->route[0].'.php';
+        if ($this->isAcl('all')) {
+            return true;
+        }
+        elseif (isset($_SESSION['authorized']['id']) && $this->isAcl('authorize')) {
+            return true;
+        }
+        elseif (!isset($_SESSION['authorized']['id']) && $this->isAcl('guest')) {
+            return true;
+        }
+        elseif (isset($_SESSION['admin']) && $this->isAcl('admin')) {
+            return true;
+        }
+        else return false;
+
+    }
+
+    public function isAcl($key) {
+
+        return in_array($this->route[1], $this->acl[$key]);
 
     }
 
